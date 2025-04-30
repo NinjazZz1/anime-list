15d6f105729516d66b2581f17c467bef9138cbd9
NinjazZz1 <85305843+NinjazZz1@users.noreply.github.com>
2025-05-01 00:14:28 +0100

    tinkered with forms a little

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function Anime(name, episodes, completedEpisodes, type) {
     this.episodes = episodes;
     this.completedEpisodes = completedEpisodes;
     this.type = type;
-    id = crypto.randomUUID();
+    this.id = crypto.randomUUID();
 
     this.info = function() {
         return this.name
@@ -17,25 +17,31 @@ function Anime(name, episodes, completedEpisodes, type) {
 function addAnimeToCollection(name, episodes, completedEpisodes, type) {
     const animeToAdd = new Anime(name, episodes, completedEpisodes, type);
     myCollection.push(animeToAdd);
-
-    const section = document.createElement("tr");
-    table.appendChild(section);
-
-    const title = document.createElement("td");
-    title.innerHTML = name;
-    section.append(title);
-
-    const totalEpisodes = document.createElement("td")
-    totalEpisodes.innerHTML = episodes.toString() + "/" + completedEpisodes.toString();
-    totalEpisodes.classList.add("center");
-    section.appendChild(totalEpisodes);
-
-    const animeType = document.createElement("td")
-    animeType.innerHTML = type;
-    animeType.classList.add("center");
-    section.appendChild(animeType);
 }
 
 addAnimeToCollection("日常", 26, 26, "TV");
 addAnimeToCollection("空の境界 俯瞰風景", 1, 1, "Movie");
 addAnimeToCollection("アマガミSS", 25, 25, "TV");
+
+addToTable();
+
+function addToTable() {
+    for (let i = 0; i < myCollection.length; i++) {
+        const section = document.createElement("tr");
+        table.appendChild(section);
+
+        const title = document.createElement("td");
+        title.innerHTML = myCollection[i].name;
+        section.append(title);
+
+        const totalEpisodes = document.createElement("td")
+        totalEpisodes.innerHTML = myCollection[i].episodes.toString() + "/" + myCollection[i].completedEpisodes.toString();
+        totalEpisodes.classList.add("center");
+        section.appendChild(totalEpisodes);
+
+        const animeType = document.createElement("td")
+        animeType.innerHTML = myCollection[i].type;
+        animeType.classList.add("center");
+        section.appendChild(animeType);
+    }
+}
